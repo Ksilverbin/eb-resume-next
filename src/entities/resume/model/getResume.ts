3b839bdff7a532cf4de32adf8e6869d4febe44b9
Ksilverbin <kiobn1201@gmail.com>
2026-04-16 22:28:07 +0900

    refactor: split resume model into domain json files

--- src/entities/resume/model/getResume.ts
+++ src/entities/resume/model/getResume.ts
@@ -1,6 +1,25 @@
-import resumeJson from "./resume.json";
-import type { ResumeData } from "./types";
+import certificatesJson from "./certificates.json";
+import educationsJson from "./educations.json";
+import experiencesJson from "./experiences.json";
+import profileJson from "./profile.json";
+import projectsJson from "./projects.json";
+import skillsJson from "./skills.json";
+import type {
+  Certificate,
+  Education,
+  Experience,
+  Profile,
+  Project,
+  ResumeData
+} from "./types";
 
 export function getResume(): ResumeData {
-  return resumeJson as ResumeData;
+  return {
+    ...(profileJson as Profile),
+    experiences: experiencesJson as Experience[],
+    projects: projectsJson as Project[],
+    skills: skillsJson as string[],
+    educations: educationsJson as Education[],
+    certificates: certificatesJson as Certificate[]
+  };
 }
